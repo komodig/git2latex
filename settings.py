f65ab18fb03b885edb48d54d05a6c844ab8637cc
Lutz <python@tastybyte.de>
2021-02-11 03:00:37 +0100

    updated settings with project g2l

--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ import os
 
 PROJECTS = [
     {
-        'workspace': os.path.join('/', 'home', 'karl', 'workspace', 'reeknersprook'),
-        'author': 'Lutz Ballaschke',
-        'name': 'rs'
+        'workspace': os.path.join('/', 'home', 'karl', 'workspace', 'git2latex'),
+        'author': 'Lutz',
+        'name': 'g2l'
     },
     {
         'workspace': os.path.join('/', 'home', 'karl', 'workspace', 'avr-uno'),
@@ -15,7 +15,7 @@ PROJECTS = [
     }
 ]
 START_DATE = '2018-01-01'
-END_DATE = '2018-12-31'
+END_DATE = '2021-02-11'
 DAILY_HOURS = 8
 HOURLY_RATE = 80
 VAT_PERCENT = 16
